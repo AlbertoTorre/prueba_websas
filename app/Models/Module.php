703409fee115e9d4d06b1bf6151260823c0e7d18
root <albertochoatorres@gmail.com>
2018-09-20 16:55:00 -0500

    Creacion de migración y archivo modelo de tabla.

---
+++ app/Models/Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Module extends Model
+{
+	use SoftDeletes;
+
+	protected $fillable = [ 
+		'parent_id',
+		'name',
+		'url',
+		'order',
+		'active'
+	];
+	
+	protected $dates = ['deleted_at'];
+}
---
+++ database/migrations/2018_09_20_201116_create_modules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->default(0);
+            $table->string('name', 150);
+            $table->string('url',255)->default('#');
+            $table->integer('order')->default(0);
+            $table->enum('active',[0,1]);
+            $table->timestamps();
+            
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modules');
+    }
+}
